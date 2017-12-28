56d101cb45728f9428024359e6b5401b1f0c4b6d
Julie Rybolt <jrybolt73@gmail.com>
2017-12-28 10:58:49 -0600

    lesson 3

---
+++ crowdfund.rb
@@ -0,0 +1,10 @@
+project1 = "abc"
+project2 = "lmn"
+project3 = "xyz"
+
+funding1 = "1000"
+funding2 = "500"
+funding3 = "25"
+
+puts "Project #{project1.upcase} has $#{funding1} in funding"
+puts "Projects: \n\t#{project1.upcase}\n\t#{project2.upcase}\n\t#{project1.upcase}"
